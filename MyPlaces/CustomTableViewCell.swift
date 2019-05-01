c0e45c1fad6362a984557e510ae9eb1a99f48e2e
Ilya <marckellow@icloud.com>
2019-05-01 20:25:38 +0300

    Custom table view cell

---
+++ MyPlaces/CustomTableViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  CustomTableViewCell.swift
+//  MyPlaces
+//
+//  Created by Илья Маркелов on 01/05/2019.
+//  Copyright © 2019 Илья Маркелов. All rights reserved.
+//
+
+import UIKit
+
+class CustomTableViewCell: UITableViewCell {
+
+    @IBOutlet var imageOfPlace: UIImageView!
+    @IBOutlet var nameLabel: UILabel!
+    @IBOutlet var locationLabel: UILabel!
+    @IBOutlet var typeLabel: UILabel!
+    
+}
--- MyPlaces/MainViewController.swift
+++ MyPlaces/MainViewController.swift
@@ -26,12 +26,12 @@ class MainViewController: UITableViewController {
 
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath) as! CustomTableViewCell
 
-        cell.textLabel?.text = fastFood[indexPath.row]
-        cell.imageView?.image = UIImage(named: fastFood[indexPath.row])
-        cell.imageView?.layer.cornerRadius = cell.frame.size.height / 2 // устанавливаем размер ячейки
-        cell.imageView?.clipsToBounds = true // округялем учейки
+        cell.nameLabel.text = fastFood[indexPath.row]
+        cell.imageOfPlace.image = UIImage(named: fastFood[indexPath.row])
+        cell.imageOfPlace.layer.cornerRadius = cell.imageOfPlace.frame.size.height / 2 // устанавливаем размер ячейки
+        cell.imageOfPlace.clipsToBounds = true // округялем учейки
 
         return cell
     }
